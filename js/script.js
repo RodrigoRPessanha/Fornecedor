bf79c362aea068616641e357d7c2e863e46034b8
RodrigoRPessanha <96801642+RodrigoRPessanha@users.noreply.github.com>
2023-04-10 12:48:50 -0300

    Mudanças na NavBar e AsideBar

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
+const aside = document.querySelector(".aside");
+const menu = document.querySelector("#menu");
+const burguerMenu = document.querySelector("#burguer-menu");
+const main = document.querySelector("#main");
+aside.style.width = "1.5vw";
+menu.style.display = "none";
+burguerMenu.innerHTML = "menu";
+main.style.marginLeft = "4vw";
+
 function burguerClicked() {
-  const menu = document.querySelector("#menu");
-  const aside = document.querySelector(".aside");
   if (menu.style.display == "block") {
     menu.style.display = "none";
-    aside.style.width = "50px";
+    aside.style.width = "1.5vw";
+    burguerMenu.innerHTML = "menu";
+    main.style.marginLeft = "4vw";
   } else {
     menu.style.display = "block";
-    aside.style.width = "200px";
+    aside.style.width = "12vw";
+    burguerMenu.innerHTML = "close";
+    main.style.marginLeft = "14.5vw";
   }
 }
