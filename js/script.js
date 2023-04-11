d10268e216625dee824df8e69065846bdf0e0fbb
RodrigoRPessanha <96801642+RodrigoRPessanha@users.noreply.github.com>
2023-04-11 14:43:40 -0300

    Melhorias na Tela Incial

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 const aside = document.querySelector(".aside");
 const menu = document.querySelector("#menu");
+const menuIcones = document.querySelector("#menu-icones");
 const burguerMenu = document.querySelector("#burguer-menu");
 const main = document.querySelector("#main");
-aside.style.width = "1.5vw";
+aside.style.width = "40px";
 menu.style.display = "none";
+menuIcones.style.display = "block";
 burguerMenu.innerHTML = "menu";
-main.style.marginLeft = "4vw";
+main.style.marginLeft = "65px";
 
 function burguerClicked() {
   if (menu.style.display == "block") {
     menu.style.display = "none";
-    aside.style.width = "1.5vw";
+    menuIcones.style.display = "block";
+    aside.style.width = "40px";
     burguerMenu.innerHTML = "menu";
-    main.style.marginLeft = "4vw";
+    main.style.marginLeft = "65px";
   } else {
     menu.style.display = "block";
-    aside.style.width = "12vw";
+    menuIcones.style.display = "none";
+    aside.style.width = "220px";
     burguerMenu.innerHTML = "close";
-    main.style.marginLeft = "14.5vw";
+    main.style.marginLeft = "265px";
   }
 }
