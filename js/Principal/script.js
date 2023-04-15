01aedcef71861b6d578c3228b4493f9667cb19e8
RodrigoRPessanha <96801642+RodrigoRPessanha@users.noreply.github.com>
2023-04-15 18:45:49 -0300

    css modificado para grid + flex

--- js/Principal/script.js
+++ js/Principal/script.js
@@ -1,26 +1,26 @@
-const aside = document.querySelector(".aside");
+const content = document.querySelector(".content");
 const menu = document.querySelector("#menu");
 const menuIcones = document.querySelector("#menu-icones");
 const burguerMenu = document.querySelector("#burguer-menu");
-const main = document.querySelector("#main");
-aside.style.width = "40px";
-menu.style.display = "none";
-menuIcones.style.display = "block";
-burguerMenu.innerHTML = "menu";
-main.style.marginLeft = "65px";
+iniciar();
 
 function burguerClicked() {
   if (menu.style.display == "block") {
     menu.style.display = "none";
     menuIcones.style.display = "block";
-    aside.style.width = "40px";
     burguerMenu.innerHTML = "menu";
-    main.style.marginLeft = "65px";
+    content.style.gridTemplateColumns = "1fr 30fr";
   } else {
     menu.style.display = "block";
     menuIcones.style.display = "none";
-    aside.style.width = "220px";
     burguerMenu.innerHTML = "close";
-    main.style.marginLeft = "265px";
+    content.style.gridTemplateColumns = "1fr 6.5fr";
   }
+}
+
+function iniciar() {
+  content.style.gridTemplateColumns = "1fr 30fr";
+  menu.style.display = "none";
+  menuIcones.style.display = "block";
+  burguerMenu.innerHTML = "menu";
 }
